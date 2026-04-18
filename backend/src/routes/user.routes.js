fa37b409038c85be39d6fee4e226793ef57ef112
vikramChaurasiya <bhartivikram633@gmail.com>
2026-04-18 13:31:39 +0530

    feat: add user profile route to retrieve current user information

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, registerUser,logoutUser,refereshAccessToken, changePassword, getAllUsers, updateUser, deleteUser } from "../controllers/user.controllers.js";
+import { loginUser, registerUser,logoutUser,refereshAccessToken, changePassword, getAllUsers, updateUser, deleteUser, getCurrentUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { AuthorizeRole } from "../middlewares/role.middleware.js";
@@ -20,6 +20,7 @@ router.route("/logout").post(verifyJWT ,logoutUser);
 router.route("/refresh-token").post(refereshAccessToken)
 router.put("/change-password", verifyJWT, changePassword)
 router.route("/all-users").get(verifyJWT, AuthorizeRole("admin"), getAllUsers)
+router.route("/user-profile").get(verifyJWT, getCurrentUser)
 router.route("/update/:id").put(verifyJWT, updateUser)
 router.route("/delete/:id").delete(verifyJWT, deleteUser)
 
